03b931d8a7588d657142f84b2713a66bb5ff1a6c
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2025-07-26 00:43:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+
+    static class Pair {
+        int node, weight;
+        Pair(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+    }
+
+    public int[] dijkstra(int V, int[][] edges, int src) {
+        // Build adjacency list
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1], wt = edge[2];
+            adj.get(u).add(new Pair(v, wt));
+            adj.get(v).add(new Pair(u, wt)); // Remove this line for directed graph
+        }
+
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+        pq.add(new Pair(src, 0));
+
+        while (!pq.isEmpty()) {
+            Pair current = pq.poll();
+            int u = current.node;
+            int d = current.weight;
+
+            for (Pair neighbor : adj.get(u)) {
+                int v = neighbor.node;
+                int wt = neighbor.weight;
+                if (dist[u] + wt < dist[v]) {
+                    dist[v] = dist[u] + wt;
+                    pq.add(new Pair(v, dist[v]));
+                }
+            }
+        }
+
+        return dist;
+    }
+}
