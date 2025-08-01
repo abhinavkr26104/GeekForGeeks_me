7630538cde15325b69d6285cb4fbd456109ad192
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2025-08-01 10:58:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,39 +1,50 @@
 import java.util.*;
 
 class Solution {
-    static int spanningTree(int V, int E, List<List<int[]>> adj) {
-        boolean[] selected = new boolean[V];
-        int[] minWeight = new int[V];
-        Arrays.fill(minWeight, Integer.MAX_VALUE);
-        minWeight[0] = 0;
+    static class Edge implements Comparable<Edge> {
+        int u, v, w;
+        Edge(int u, int v, int w) { this.u = u; this.v = v; this.w = w; }
+        public int compareTo(Edge o) { return Integer.compare(this.w, o.w); }
+    }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.add(new int[]{0, 0});
+    static class DSU {
+        int[] parent, rank;
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+        int find(int x) {
+            if (parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+        boolean union(int a, int b) {
+            int pa = find(a), pb = find(b);
+            if (pa == pb) return false;
+            if (rank[pa] < rank[pb]) parent[pa] = pb;
+            else if (rank[pb] < rank[pa]) parent[pb] = pa;
+            else {
+                parent[pb] = pa;
+                rank[pa]++;
+            }
+            return true;
+        }
+    }
 
+    public int spanningTree(int V, int[][] edges) {
+        List<Edge> list = new ArrayList<>();
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            list.add(new Edge(u, v, w));
+        }
+        Collections.sort(list);
+        DSU dsu = new DSU(V);
         int totalWeight = 0;
-
-        for (int i = 0; i < V; ) {
-            if (pq.isEmpty()) return -1;
-
-            int[] top = pq.poll();
-            int weight = top[0];
-            int u = top[1];
-
-            if (selected[u]) continue;
-
-            selected[u] = true;
-            totalWeight += weight;
-            i++;
-
-            for (int[] edge : adj.get(u)) {
-                int v = edge[0], w = edge[1];
-                if (!selected[v] && w < minWeight[v]) {
-                    minWeight[v] = w;
-                    pq.add(new int[]{w, v});
-                }
+        for (Edge e : list) {
+            if (dsu.union(e.u, e.v)) {
+                totalWeight += e.w;
             }
         }
-
         return totalWeight;
     }
 }
